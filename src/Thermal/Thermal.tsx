167b11f9e1cb1f51e78b5e6a75a10cd2618acdc6
Thomas Lefebvre <thmslbvr@gmail.com>
2023-01-22 17:23:42 -0500

    Create group for thermals

---
+++ src/Thermal/Thermal.tsx
@@ -0,0 +1,18 @@
+import { ThermalGauge, Props as ThermalGaugeProps } from "../ThermalGauge";
+import styles from "./Thermal.css";
+
+type Props = {
+  title: string;
+};
+
+export const Thermal: React.FC<Pick<ThermalGaugeProps, "degrees"> & Props> = ({
+  degrees,
+  title,
+}) => {
+  return (
+    <div>
+      <h2 className={styles.title}>{title}</h2>
+      <ThermalGauge degrees={degrees} size={200} />;
+    </div>
+  );
+};
