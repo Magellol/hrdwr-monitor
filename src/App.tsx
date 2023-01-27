07c2cde04c940830925703b8848bd9116c808b53
Thomas Lefebvre <thmslbvr@gmail.com>
2023-01-26 20:55:50 -0500

    System statuses

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,65 @@
 import "normalize.css";
 import "./Globals.css";
 import * as O from "fp-ts/Option";
+import * as Sum from "@unsplash/sum-types";
 import * as React from "react";
 import { Thermal } from "./Thermal";
 import { Block } from "./Block";
 import styles from "./App.css";
+import { constant, constNull, flow, pipe } from "fp-ts/es6/function";
+import classNames from "classnames";
+
+type Status = Sum.Member<"OK"> | Sum.Member<"Warning"> | Sum.Member<"Critical">;
+const Status = Sum.create<Status>();
+
+const cpuMin = 30;
+const cpuMax = 70;
+
+const gpuMin = 40;
+const gpuMax = 80;
 
 export const App: React.FC = () => {
+  const [status, setStatus] = React.useState<O.Option<Status>>(O.none);
   const [degrees, setDegrees] = React.useState(30);
+
+  React.useEffect(() => {
+    if (degrees <= cpuMax) {
+      setStatus(O.some(Status.mk.OK));
+    } else if (degrees > cpuMax && degrees < cpuMax + 25) {
+      setStatus(O.some(Status.mk.Warning));
+    } else {
+      setStatus(O.some(Status.mk.Critical));
+    }
+  }, [degrees]);
 
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.statusContainer}>
-          <div className={styles.status} />
-          <span className={styles.systemStatus}>Systems operational</span>
+          {pipe(
+            status,
+            O.map(
+              flow(
+                Status.match<[string, string]>({
+                  OK: constant(["Systems operating normally", styles.green]),
+                  Warning: constant(["Systems degraded", styles.orange]),
+                  Critical: constant([
+                    "Systems in critical condition",
+                    styles.red,
+                  ]),
+                }),
+                ([copy, classes]) => (
+                  <>
+                    <div className={classNames(styles.status, classes)} />
+                    <span className={styles.systemStatus}>{copy}</span>
+                  </>
+                )
+              )
+            ),
+
+            // TODO: make a better placeholder
+            O.getOrElseW(() => null)
+          )}
         </div>
       </header>
       <div className={styles.layout}>
