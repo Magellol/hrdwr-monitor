f0e02a19a6b3f748c915bc94cd2da5595433d944
Thomas Lefebvre <thmslbvr@gmail.com>
2023-01-28 21:35:48 -0500

    Remove global status for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,22 @@
-import "normalize.css";
-import "./Globals.css";
 import * as O from "fp-ts/Option";
-import * as Sum from "@unsplash/sum-types";
+import "normalize.css";
 import * as React from "react";
-import { Thermal } from "./Thermal";
-import { Block } from "./Block";
 import styles from "./App.css";
-import { constant, constNull, flow, pipe } from "fp-ts/es6/function";
-import classNames from "classnames";
-
-type Status = Sum.Member<"OK"> | Sum.Member<"Warning"> | Sum.Member<"Critical">;
-const Status = Sum.create<Status>();
-
-const cpuMin = 30;
-const cpuMax = 70;
-
-const gpuMin = 40;
-const gpuMax = 80;
+import { Block } from "./Block";
+import "./Globals.css";
+import { Thermal } from "./Thermal";
 
 export const App: React.FC = () => {
-  const [status, setStatus] = React.useState<O.Option<Status>>(O.none);
   const [degrees, setDegrees] = React.useState(30);
-
-  React.useEffect(() => {
-    if (degrees <= cpuMax) {
-      setStatus(O.some(Status.mk.OK));
-    } else if (degrees > cpuMax && degrees < cpuMax + 25) {
-      setStatus(O.some(Status.mk.Warning));
-    } else {
-      setStatus(O.some(Status.mk.Critical));
-    }
-  }, [degrees]);
 
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        <div className={styles.statusContainer}>
-          {pipe(
-            status,
-            O.map(
-              flow(
-                Status.match<[string, string]>({
-                  OK: constant(["Systems operating normally", styles.green]),
-                  Warning: constant(["Systems degraded", styles.orange]),
-                  Critical: constant([
-                    "Systems in critical condition",
-                    styles.red,
-                  ]),
-                }),
-                ([copy, classes]) => (
-                  <>
-                    <div className={classNames(styles.status, classes)} />
-                    <span className={styles.systemStatus}>{copy}</span>
-                  </>
-                )
-              )
-            ),
-
-            // TODO: make a better placeholder
-            O.getOrElseW(() => null)
-          )}
-        </div>
-      </header>
       <div className={styles.layout}>
         <Block background={O.none}>
-          <Thermal degrees={degrees} title="CPU Core" min={30} max={75} />
+          <Thermal degrees={degrees} title="CPU Core" />
         </Block>
         <Block background={O.none}>
-          <Thermal degrees={degrees} title="GPU Core" min={45} max={80} />
+          <Thermal degrees={degrees} title="GPU Core" />
         </Block>
       </div>
       <input
