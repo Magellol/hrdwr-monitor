5c87b7f2ae5f392aa2e1bab747d8f5b717c777ab
Thomas Lefebvre <thmslbvr@gmail.com>
2023-01-29 16:37:30 -0500

    Fix type

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -140,7 +140,7 @@ declare module "gaugeJS" {
      *    </code>
      * @see http://jsfiddle.net/berni/Yb4d7/
      */
-    percentColors?: [[number, string]];
+    percentColors?: Array<[number, string]>;
 
     /**
      * Value labels
