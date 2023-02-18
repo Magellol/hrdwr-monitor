3ed168e3a6e5cb94ce590d575b921466e089ca31
Thomas Lefebvre <thmslbvr@gmail.com>
2023-02-18 18:19:54 -0500

    Reimagine the layout; I wasn't too happy with the previous one

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import classNames from "classnames";
 import "normalize.css";
 import * as React from "react";
 import styles from "./App.css";
-import { Block } from "./Block";
 import "./Globals.css";
 import { Thermal } from "./Thermal";
-import { UsageGauge } from "./UsageGauge/UsageGauge";
 import { pathSample1, pathSample2 } from "./ThermalGauge";
 
 export const App: React.FC = () => {
@@ -13,35 +12,33 @@ export const App: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.layout}>
-        <Block
-          containerClassName={styles.gaugesBlock}
-          className={styles.gaugesContent}
-          title="Load"
-        >
-          <UsageGauge
-            min={0}
-            max={24000}
-            n={15000}
-            unit="MBs"
-            title="GPU VRAM"
-          />
-          <UsageGauge
-            min={0}
-            max={10000}
-            n={3000}
-            unit="Hz"
-            title="GPU Clock"
-          />
-          <UsageGauge min={0} max={32000} n={3000} unit="MBs" title="DRAM" />
-        </Block>
-        <Block containerClassName={styles.fansBlock} title="Fan speeds"></Block>
-        <div className={styles.thermalBlock}>
-          <Block title="CPU Core">
-            <Thermal degrees={degrees} id="cpu-core" paths={pathSample1} />
-          </Block>
-          <Block title="GPU Core">
-            <Thermal degrees={degrees} id="gpu-core" paths={pathSample2} />
-          </Block>
+        <Thermal degrees={degrees} label="CPU Core" paths={pathSample1} />
+        <Thermal degrees={degrees} label="GPU Core" paths={pathSample2} />
+        <div className={styles.bgPattern}>
+          <div
+            className={classNames(
+              styles.fade,
+              styles.sideFade,
+              styles.leftFade
+            )}
+          ></div>
+          <div
+            className={classNames(
+              styles.fade,
+              styles.sideFade,
+              styles.rightFade
+            )}
+          ></div>
+          <div
+            className={classNames(styles.fade, styles.baseFade, styles.topFade)}
+          ></div>
+          <div
+            className={classNames(
+              styles.fade,
+              styles.baseFade,
+              styles.bottomFade
+            )}
+          ></div>
         </div>
       </div>
       <input
