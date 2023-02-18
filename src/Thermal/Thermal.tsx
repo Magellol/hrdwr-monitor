51ab9ad2c31d17e07dd8f9415c180a8f6dddf9e9
Thomas Lefebvre <thmslbvr@gmail.com>
2023-02-18 09:13:24 -0500

    Clean up thermal

--- src/Thermal/Thermal.tsx
+++ src/Thermal/Thermal.tsx
@@ -1,25 +1,21 @@
 import classNames from "classnames";
-import { Props as ThermalGaugeProps, ThermalGauge } from "../ThermalGauge";
+import {
+  Props as ThermalGaugeProps,
+  ThermalGauge
+} from "../ThermalGauge";
 import styles from "./Thermal.css";
 
-type Props = {
-  title: string;
-};
-
-export const Thermal: React.FC<Pick<ThermalGaugeProps, "degrees"> & Props> = ({
-  degrees,
-  title,
-}) => {
+export const Thermal: React.FC<
+  Pick<ThermalGaugeProps, "degrees" | "paths"> & { id: string }
+> = ({ id, degrees, paths }) => {
   return (
     <div className={styles.container}>
       <div className={classNames(styles.content)}>
-        <header className={styles.header}>
-          <h2 className={styles.title}>{title}</h2>
-        </header>
         <ThermalGauge
           degrees={degrees}
-          size={175}
-          id={title}
+          size={225}
+          paths={paths}
+          id={id}
           min={30}
           max={85}
         />
