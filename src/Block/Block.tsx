5ff42e0439bca535206afed975136268b18a4c82
Thomas Lefebvre <thmslbvr@gmail.com>
2023-02-18 09:13:30 -0500

    Allow block to take a title

--- src/Block/Block.tsx
+++ src/Block/Block.tsx
@@ -3,10 +3,15 @@ import classNames from "classnames";
 
 export const Block: React.FC<{
   children: React.ReactNode;
+  title: string;
   className?: string;
-}> = ({ children, className }) => (
-  <div className={classNames(styles.container, className)}>
+  containerClassName?: string;
+}> = ({ children, title, className, containerClassName }) => (
+  <div className={classNames(styles.container, containerClassName)}>
     {/* <div className={styles.bg} /> */}
-    {children}
+    <header className={styles.header}>
+      <h2 className={styles.title}>{title}</h2>
+    </header>
+    <div className={classNames(styles.content, className)}>{children}</div>
   </div>
 );
