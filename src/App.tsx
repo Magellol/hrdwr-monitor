92b8cccde05ee8168cc97c48023404631adfb9c1
Thomas Lefebvre <thmslbvr@gmail.com>
2023-02-26 16:14:09 -0500

    Add connecting line between the two thermals

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,33 @@ import "./Globals.css";
 import { Thermal, Dir } from "./Thermal";
 import { pathSample1, pathSample2 } from "./ThermalGauge";
 
+const ConnectingLine: React.FC = () => {
+  const id = React.useId();
+  return (
+    <svg
+      width="222"
+      height="37"
+      viewBox="0 0 222 37"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <linearGradient id={id}>
+          <stop stopColor="transparent" offset="0%" />
+          <stop stopColor="white" offset="30%" />
+        </linearGradient>
+      </defs>
+      {/* Note: this has to closely related to the same path in the`styles.connectingLine svg path */}
+      <path
+        stroke={`url(#${id})`}
+        d="M0 1H186.161L221 36"
+        className={styles.connectingLinePath}
+        vectorEffect="non-scaling-stroke"
+      />
+    </svg>
+  );
+};
+
 export const App: React.FC = () => {
   const [degrees, setDegrees] = React.useState(35);
 
@@ -20,6 +47,37 @@ export const App: React.FC = () => {
           paths={pathSample1}
           dir={Dir.mk.Left}
         />
+        <div className={styles.connectingLines}>
+          <div
+            className={classNames(
+              styles.connectingLineContainer,
+              styles.leftConnectingLineContainer
+            )}
+          >
+            <div
+              className={classNames(
+                styles.connectingLine,
+                styles.leftConnectingLine
+              )}
+            />
+            <ConnectingLine />
+          </div>
+          <div
+            className={classNames(
+              styles.connectingLineContainer,
+              styles.rightConnectingLineContainer
+            )}
+          >
+            <div
+              className={classNames(
+                styles.connectingLine,
+                styles.rightConnectingLine
+              )}
+            />
+            <ConnectingLine />
+          </div>
+        </div>
+
         <Thermal
           degrees={degrees}
           label="GPU Core"
