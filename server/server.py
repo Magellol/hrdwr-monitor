a617ab59077b63a6f31093f3728ff7620daa31da
Thomas Lefebvre <thmslbvr@gmail.com>
2023-03-18 21:45:32 -0400

    Remove unecessary cors config

--- server/server.py
+++ server/server.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_cors import CORS, cross_origin
+from flask_cors import cross_origin
 import psutil
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/ps")
 @cross_origin()
