6ff147731d095df6066e5e9f9033fe6f6e5cc05b
Thomas Lefebvre <thmslbvr@gmail.com>
2023-03-25 15:20:43 -0400

    Setup jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type import("jest").Config
+ */
+const cfg = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': ['es-jest', { jsx: 'automatic' }],
+  },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  modulePaths: ['<rootDir>'],
+  moduleFileExtensions: ['ts'],
+};
+
+module.exports = cfg;
