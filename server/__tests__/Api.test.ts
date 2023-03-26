baeb38e4bb6662fe3307a9586c4810808dbbc709
Thomas Lefebvre <thmslbvr@gmail.com>
2023-03-26 18:57:12 -0400

    Write failing tests

--- server/__tests__/Api.test.ts
+++ server/__tests__/Api.test.ts
@@ -1,19 +1,34 @@
-import { normalize, ResponseCodec } from "../Api";
 import * as E from "fp-ts/Either";
-import data from "./data";
+import * as O from "fp-ts/Option";
+import { ResponseCodec } from "../Api";
 
 describe("Api", () => {
   describe("normalize", () => {
-
-    // TODO: test with actual `data`
-    it("works", () => {
+    it("fallbacks when decoding failed", () => {
       const decoded = ResponseCodec.decode([
         {
           SensorApp: "HWiNFO",
           SensorClass: "CPU [#0]: Intel Core i5-13600K: DTS",
+
+          // Unsupported sensor name for HWiNFO sensor app.
           SensorName: "P-core 0",
           SensorValue: "46",
-          SensorUnit: "C",
+          SensorUnit: "Â°C",
+          SensorUpdateTime: 1679766815,
+        },
+      ]);
+
+      expect(decoded).toEqual(E.right(O.none));
+    });
+
+    it("sanitizes before decoding", () => {
+      const decoded = ResponseCodec.decode([
+        {
+          SensorApp: "HWiNFO",
+          SensorClass: "CPU [#0]: Intel Core i5-13600K: DTS",
+          SensorName: "CPU Package",
+          SensorValue: "46",
+          SensorUnit: "Â°C",
           SensorUpdateTime: 1679766815,
         },
       ]);
