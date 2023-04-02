a429470fef33442d1380386e80815a9c8699aaee
Thomas Lefebvre <thmslbvr@gmail.com>
2023-04-01 20:24:11 -0400

    Add error handling

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,30 +1,39 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-use exitfailure::ExitFailure;
 use reqwest::Url;
 use serde_derive::{Deserialize, Serialize};
 
-#[derive (Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Sensor {
-  SensorName: String,
+    SensorName: String,
+}
+
+#[derive(Serialize)]
+enum SensorError {
+    Fetch,
+    Decode
 }
 
 #[tauri::command]
-async fn get_sensor() -> Result<Vec<Sensor>, String> {
-  let url = std::env::var("SENSOR_HOST").expect("Missing SENSOR_HOST env var");
-  let url = Url::parse(&url).expect("Bad url");
+async fn get_sensor() -> Result<Vec<Sensor>, SensorError> {
+    let url = std::env::var("SENSOR_HOST").expect("Missing SENSOR_HOST env var");
+    let url = Url::parse(&url).expect("Bad url");
 
-  // TODO: error handling
-  let res = reqwest::get(url).await.expect("Failed request").json::<Vec<Sensor>>().await.expect("not json");
-  Ok(res)
+    let res = reqwest::get(url)
+        .await
+        .map_err(|_err| SensorError::Fetch)?
+        .json::<Vec<Sensor>>()
+        .await
+        .map_err(|_err| SensorError::Decode)?;
+    Ok(res)
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![get_sensor])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_sensor])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
