e7f93bc599823ad3af5fd41db9df75bb0614576c
Thomas Lefebvre <thmslbvr@gmail.com>
2023-04-06 13:45:07 -0400

    fmt

--- flake.nix
+++ flake.nix
@@ -4,12 +4,13 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = {
-      nixpkgs,
-      rust-overlay,
-      ...
-    }: let
-      overlays = [(import rust-overlay)];
+  outputs =
+    { nixpkgs
+    , rust-overlay
+    , ...
+    }:
+    let
+      overlays = [ (import rust-overlay) ];
 
       # Helper generating outputs for each desired system
       forAllSystems = nixpkgs.lib.genAttrs [
@@ -17,36 +18,40 @@
       ];
 
       # Import nixpkgs' package set for each system.
-      nixpkgsFor = forAllSystems(system:
+      nixpkgsFor = forAllSystems (system:
         import nixpkgs {
           inherit system overlays;
         });
-      in {
-        devShells = forAllSystems (system: let
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
 
-        packages-darwin = with pkgs; [
-          rustup
-          nodejs-18_x
-          yarn
-          just
-          libiconv
-          darwin.apple_sdk.frameworks.Security
-          darwin.apple_sdk.frameworks.CoreServices
-          darwin.apple_sdk.frameworks.CoreFoundation
-          darwin.apple_sdk.frameworks.Foundation
-          darwin.apple_sdk.frameworks.AppKit
-          darwin.apple_sdk.frameworks.WebKit
-          darwin.apple_sdk.frameworks.Cocoa
-        ];
+          packages-darwin = with pkgs; [
+            rustup
+            nodejs-18_x
+            yarn
+            just
+            libiconv
+            nixpkgs-fmt
+            darwin.apple_sdk.frameworks.Security
+            darwin.apple_sdk.frameworks.CoreServices
+            darwin.apple_sdk.frameworks.CoreFoundation
+            darwin.apple_sdk.frameworks.Foundation
+            darwin.apple_sdk.frameworks.AppKit
+            darwin.apple_sdk.frameworks.WebKit
+            darwin.apple_sdk.frameworks.Cocoa
+          ];
 
-        packages = packages-darwin;
-    in {
-        default = pkgs.mkShell {
-          buildInputs = packages;
-        };
-      });
+          packages = packages-darwin;
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = packages;
+          };
+        });
     };
 }
