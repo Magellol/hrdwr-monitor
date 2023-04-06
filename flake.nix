813f19c1ddb51f66cca0d25bb06926a9430b788b
Thomas Lefebvre <thmslbvr@gmail.com>
2023-04-06 13:38:46 -0400

    Use flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = {
+      nixpkgs,
+      rust-overlay,
+      ...
+    }: let
+      overlays = [(import rust-overlay)];
+
+      # Helper generating outputs for each desired system
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+      ];
+
+      # Import nixpkgs' package set for each system.
+      nixpkgsFor = forAllSystems(system:
+        import nixpkgs {
+          inherit system overlays;
+        });
+      in {
+        devShells = forAllSystems (system: let
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        packages-darwin = with pkgs; [
+          rustup
+          nodejs-18_x
+          yarn
+          just
+          libiconv
+          darwin.apple_sdk.frameworks.Security
+          darwin.apple_sdk.frameworks.CoreServices
+          darwin.apple_sdk.frameworks.CoreFoundation
+          darwin.apple_sdk.frameworks.Foundation
+          darwin.apple_sdk.frameworks.AppKit
+          darwin.apple_sdk.frameworks.WebKit
+          darwin.apple_sdk.frameworks.Cocoa
+        ];
+
+        packages = packages-darwin;
+    in {
+        default = pkgs.mkShell {
+          buildInputs = packages;
+        };
+      });
+    };
+}
