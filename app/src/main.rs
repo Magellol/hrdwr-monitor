8092f2be66dda96397c9f8a54fc3f4fb1fe9dcfb
Thomas Lefebvre <thmslbvr@gmail.com>
2023-04-08 16:30:30 -0400

    WIP. Read actual sensor data

--- app/src/main.rs
+++ app/src/main.rs
@@ -9,46 +9,93 @@ use std::fs::File;
 use std::io::Read;
 
 #[derive(Deserialize, Serialize, Debug)]
-#[serde(tag = "SensorUnit")]
+// TODO: this should be "readingType" prop but we can't specify integer for internal tags in serde_json
+#[serde(tag = "unit")]
 enum Sensor {
-    #[serde(rename = "C")]
+    
+    #[serde(rename = "°C")]
     Temp {
-        #[serde(rename = "SensorValue")]
-        val: i32,
+        value: f64,
+        #[serde(rename = "labelOriginal")]
+        name: String,
     },
     #[serde(rename = "%")]
     Load {
-        #[serde(rename = "SensorValue")]
-        val: i32,
+        value: f64,
+        #[serde(rename = "labelOriginal")]
+        name: String,
     },
+    #[serde(rename = "RPM")]
+    RPM {
+        #[serde(rename = "labelOriginal")]
+        name: String,
+    }
 }
 
 #[derive(Serialize)]
 enum SensorError {
     Fetch,
-    Decode,
+    Decode(String),
+}
+
+#[derive(Serialize)]
+struct Response {
+    totalCpuLoad: f64,
+    cpuTemp: f64,
+    gpuTemp: f64
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct HWiNfo {
+    readings: Vec<Sensor>
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct RemoteResponse {
+    hwinfo: HWiNfo
 }
 
 #[tauri::command]
-async fn get_sensor() -> Result<Vec<Sensor>, SensorError> {
+async fn get_sensor() -> Result<Response, SensorError> {
     if cfg!(target_os = "macos") {
         // Note: filepath is relative to app root (where Cargo.toml lives)
         let mut file = File::open("./src/test_data.json").unwrap();
         let mut data = String::new();
         file.read_to_string(&mut data).unwrap();
 
-        let sensors: Vec<Sensor> = serde_json::from_str(&data).expect("bad json");
-        Ok(sensors)
+        let _sensors: Vec<Sensor> = serde_json::from_str(&data).expect("bad json");
+        Ok(Response {
+            totalCpuLoad: 0.0,
+            gpuTemp: 0.0,
+            cpuTemp: 0.0
+        })
     } else if cfg!(target_os = "windows") {
-        let host = std::env::var("SENSOR_HOST").expect("Missing SENSOR_HOST env var");
+        // TODO: make url configurable
+        let host = "http://localhost:5000/json";
         let url = Url::parse(&host).expect("Bad url");
 
-        let res = reqwest::get(url)
+        let rmtResponse = reqwest::get(url)
             .await
             .map_err(|_err| SensorError::Fetch)?
-            .json::<Vec<Sensor>>()
+            .json::<RemoteResponse>()
             .await
-            .map_err(|_err| SensorError::Decode)?;
+            .map_err(|_err| SensorError::Decode(_err.to_string()))?;
+        let mut iter = rmtResponse.hwinfo.readings.into_iter();
+
+        let res = Response {
+            // TODO: clean this up lol
+            totalCpuLoad: iter.find(|x| match x {
+                Sensor::Temp { name, .. } => name == "Total CPU Usage",
+                Sensor::Load { name, .. } => name == "Total CPU Usage",
+                Sensor::RPM { name, .. } => name == "Total CPU Usage"
+            }).map(|s| match s {
+                Sensor::Load { value, .. } => value,
+                Sensor::Temp { value, .. } => value,
+                Sensor::RPM {..}  => 0.0,
+            }).unwrap_or(0.0),
+            gpuTemp: 0.0,
+            cpuTemp: 0.0
+        };
         Ok(res)
     } else {
         panic!("Unknown target operating system!");
